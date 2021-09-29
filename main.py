8f79db5608fcd6e63e33ea854f2d26dc1209c0d7
“Roman <rummaan@me.com>
2021-09-28 21:44:59 -0400

    version 2.3

--- main.py
+++ main.py
@@ -50,76 +50,55 @@ def check_resources(order, enough_stuff):
             enough_stuff = False
         else:
             RESOURCES[drink_ingredient] -= drink_ingredient_quantity
-            print(f"Will use {drink_ingredient_quantity} of {drink_ingredient}")
+            # print(f"Will use {drink_ingredient_quantity} of {drink_ingredient}")
             enough_stuff = True
     return drink_ingredient, drink_ingredient_quantity, enough_stuff, RESOURCES
 
 
 def accept_coins():
     print("Please insert coins!")
-    quarters = int(input("How many quarters:  ")) * 0.25
-    print(f"inserted ${quarters} in quarters")
-    dimes = int(input("How many dimes:  ")) * 0.10
-    print(f"inserted ${dimes} in dimes")
-    nickles = int(input("How many nickles:  ")) * 0.05
-    print(f"inserted ${nickles} in nickles")
-    pennies = int(input("How many pennies:  ")) * 0.01
-    print(f"inserted ${pennies} in pennies")
-    payment = float(quarters + dimes + nickles + pennies)
+    quarters = float(input("How many quarters:  ")) * 0.25
+    # print(f"inserted ${quarters} in quarters")
+    dimes = float(input("How many dimes:  ")) * 0.10
+    # print(f"inserted ${dimes} in dimes")
+    nickles = float(input("How many nickles:  ")) * 0.05
+    # print(f"inserted ${nickles} in nickles")
+    pennies = float(input("How many pennies:  ")) * 0.01
+    # print(f"inserted ${pennies} in pennies")
+    payment = round(float(quarters + dimes + nickles + pennies), 2)
     print(f"You inserted ${payment}.")
     return payment
 
 
-def check_payment(order, payment, balance):
-    change = 0.0
-    enough_money = True
-    if order == "espresso":
-        if payment < MENU["espresso"]["cost"]:
-            enough_money = False
-        else:
-            balance += MENU["espresso"]["cost"]
-            enough_money = True
-            change = round((payment - MENU["espresso"]["cost"]), 2)
-    elif order == "latte":
-        if payment < MENU["latte"]["cost"]:
-            enough_money = False
-        else:
-            balance += MENU["latte"]["cost"]
-            enough_money = True
-            change = round((payment - MENU["latte"]["cost"]), 2)
-    elif order == "cappuccino":
-        if payment < MENU["cappuccino"]["cost"]:
-            enough_money = False
-        else:
-            balance += MENU["cappuccino"]["cost"]
-            enough_money = True
-            change = round((payment - MENU["cappuccino"]["cost"]), 2)
+def check_payment(order, payment, change, balance, enough_money):
+    if payment < MENU[order]["cost"]:
+        enough_money = False
     else:
-        print(f"no such drink as {order} exists")
-    return enough_money, order, change, balance
-
+        balance += MENU[order]["cost"]
+        enough_money = True
+        change = round((payment - MENU[order]["cost"]), 2)
+    return payment, change, balance, enough_money
 
 def recalculate_resources(order):
-
     if order == "espresso":
         RESOURCES["water"] -= MENU["espresso"]["ingredients"]["water"]
-        print(f"Water: {RESOURCES['water']}")
+        # print(f"Water: {RESOURCES['water']}")
         RESOURCES["coffee"] -= MENU["espresso"]["ingredients"]["coffee"]
-        print(f"Coffee: {RESOURCES['coffee']}")
+        # print(f"Coffee: {RESOURCES['coffee']}")
     elif order == "latte":
         RESOURCES["water"] -= MENU["latte"]["ingredients"]["water"]
-        print(f"Water: {RESOURCES['water']}")
+        # print(f"Water: {RESOURCES['water']}")
         RESOURCES["milk"] -= MENU["latte"]["ingredients"]["milk"]
-        print(f"Milk: {RESOURCES['milk']}")
+        # print(f"Milk: {RESOURCES['milk']}")
         RESOURCES["coffee"] -= MENU["latte"]["ingredients"]["milk"]
-        print(f"Coffee: {RESOURCES['coffee']}")
+        # print(f"Coffee: {RESOURCES['coffee']}")
     elif order == "cappuccino":
         RESOURCES["water"] -= MENU["cappuccino"]["ingredients"]["water"]
-        print(f"Water: {RESOURCES['water']}")
+        # print(f"Water: {RESOURCES['water']}")
         RESOURCES["milk"] -= MENU["cappuccino"]["ingredients"]["milk"]
-        print(f"Milk: {RESOURCES['milk']}")
+        # print(f"Milk: {RESOURCES['milk']}")
         RESOURCES["coffee"] -= MENU["cappuccino"]["ingredients"]["milk"]
-        print(f"Coffee: {RESOURCES['coffee']}")
+        # print(f"Coffee: {RESOURCES['coffee']}")
     else:
         print("No such drink")
     return RESOURCES
@@ -133,7 +112,6 @@ def recalculate_balance(order, balance):
             balance += balance_addition
             print(f"Balance : {balance}")
             return balance
-    return "no such drink in the machine"
 
 
 def report():
@@ -149,28 +127,29 @@ while working:
         print("Ein moment!")
         print(f"{order} was ordered")
         print(f"Price is ${MENU[order]['cost']}")
-        if order == "off":
-            print("Off was pressed")
-            working = False
-        elif order == "report":
-            print("Report was ordered")
-            report()
-        else:
-            check_resources(order, enough_stuff)
-            if enough_stuff:
-                accept_coins(order)
-                print("f{payment} in coins was accepted")
-                check_payment(order, payment, balance)
-                print(f"payment is {payment}, order is {order}, balance is {balance}")
-                if enough_money:
-                    print(f"Here is ${change} dollars in change.")
-                    print(f"Here is your {order}.Enjoy!")
-                    recalculate_balance(order, balance)
-                    print(f"order is {order}. Balance is {balance}")
-                else:
-                    print("Sorry that's not enough money. Money refunded.")
+        check_resources(order, enough_stuff)
+        if enough_stuff:
+            payment = accept_coins()
+            # print(f"{payment} in coins was accepted")
+            if payment >= MENU[order]["cost"]:
+                enough_money = True
+                balance += MENU[order]["cost"]
+                change = round((payment - MENU[order]["cost"]), 2)
+                print(f"Here is ${change} dollars in change.")
+                print(f"Here is your {order}.Enjoy!")
+                recalculate_balance(order, balance)
+                # print(f"Your order is {order}. Balance is {balance}")
             else:
-                print(f"Sorry! There is not enough resources.")
+                enough_money = False
+                print(f"Sorry {payment} is not enough. Money refunded.")
+        else:
+            print(f"Sorry! There is not enough resources.")
+    elif order == "report":
+        print("Report was ordered")
+        report()
+    elif order == "off":
+        working = False
+        print("Machine is turning off")
     else:
         print("Sorry we don't have this on the menu")
 
